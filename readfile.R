85a7401233390c4177dbaa0daffc7db7210541ec
samkart <smaj125@gmail.com>
2017-07-02 21:39:23 +0530

    Add files via upload

---
+++ readfile.R
@@ -0,0 +1,20 @@
+#' @export
+
+readfile <- function(filepath, sheet){
+  if (stringr::str_sub(filepath,(stringr::str_length(filepath)-2),-1) == "csv"){
+    read.csv(filepath)
+  } else if (stringr::str_sub(filepath,(stringr::str_length(filepath)-2),-1) == "xls" ||
+             stringr::str_sub(filepath,(stringr::str_length(filepath)-3),-1) == "xlsx") {
+      readxl::read_excel(filepath, sheet = sheet, trim_ws = TRUE)
+  } else if (stringr::str_sub(filepath,(stringr::str_length(filepath)-2),-1) == "dta"){
+    haven::read_dta(filepath)
+  } else if (stringr::str_sub(filepath,(stringr::str_length(filepath)-7),-1) == "sas7bdat"){
+    haven::read_sas(filepath)
+  }
+}
+
+# Import file function by SAM
+# Can be used to import any file, except SPSS files.
+# Who the FUCK uses SPSS anyaway?!
+# Dependencies: "stringr", "readxl", "haven"
+# "haven" used for importing SAS & STATA files
