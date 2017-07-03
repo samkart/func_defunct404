48b040e363681ce20d0759159a31c030dff8cd5a
Shubhradeep Majumdar <smaj125@gmail.com>
2017-07-03 17:46:37 +0530

    Universal import function for R
    Imports all exts, except SPSS files.

--- readfile.R
+++ readfile.R
@@ -1,11 +1,11 @@
 #' @export
 
-readfile <- function(filepath, sheet = 1){
+readfile <- function(filepath, sheet = 1, skip = 0){
   if (stringr::str_sub(filepath,(stringr::str_length(filepath)-2),-1) == "csv"){
     read.csv(filepath)
   } else if (stringr::str_sub(filepath,(stringr::str_length(filepath)-2),-1) == "xls" ||
              stringr::str_sub(filepath,(stringr::str_length(filepath)-3),-1) == "xlsx") {
-      readxl::read_excel(filepath, sheet = sheet, trim_ws = TRUE)
+      readxl::read_excel(filepath, sheet = sheet, trim_ws = TRUE, skip = skip)
   } else if (stringr::str_sub(filepath,(stringr::str_length(filepath)-2),-1) == "dta"){
     haven::read_dta(filepath)
   } else if (stringr::str_sub(filepath,(stringr::str_length(filepath)-7),-1) == "sas7bdat"){
